6199dd202edd2144b5787d1fa15282aa02493173
刘泽 <1275839779@qq.com>
2020-03-31 19:37:44 +0800

    refactor: rewrite test input and output

--- test.js
+++ test.js
@@ -1,41 +1,74 @@
 const parser = require('./parser')
 const compiler = require('./compiler')
 const fs = require('fs');
+const path = require('path');
 
 /**
  * Todos:
  * enhance test
  */
 
-const test_cases = {
-    var_nest: require('./test/var-nested.js'),
-    var_simple: require('./test/var-simple.js'),
-    extend: require('./test/extend.js'),
-    operator: require('./test/operator.js'),
-    mixin_optional_params: require('./test/mixin/optional_params.js'),
-    mixin_basic: require('./test/mixin/basic.js'),
-    mixin_var_key: require('./test/mixin/var_key.js'),
-    mixin_params: require('./test/mixin/params.js'),
+function require_css(path){
+    return fs.readFileSync(path,'utf8')
 }
 
-for(case_name in test_cases){
-    run_case(case_name,test_cases[case_name])
-}
+function run(inputDir,outputDir = './test-dist'){
+    
+    function run_test(filePath) {
+        if (path.extname(filePath) !== '.scss') return;
+        let source = require_css(filePath),
+            basename = path.basename(filePath, '.scss'),
+            dirname = path.dirname(filePath),
+            astDistPath = path.join(outputDir, 'ast', dirname.substr(4)),
+            cssDistPath = path.join(outputDir, 'css', dirname.substr(4)),
+            ast = parser(source),
+            compiled = compiler(ast);
 
-function run_case(case_name,scss){
-    let result = parser(scss)
-    fs.writeFile(`./test-result/ast-${case_name}.json`, JSON.stringify(result), function (err) {
-        if (err) {
-            console.log(`parse failed ${case_name}`)            
-            return console.error(err);
+        function write_ast() {
+            fs.writeFile(path.join(astDistPath, basename + '.json'), JSON.stringify(ast), function (err) {
+                if (err) {
+                    console.error(err)
+                    return console.error(`parse failed ${basename}`);
+                }
+            })
         }
-        // console.log(`parse success ${case_name}`)
-        fs.writeFile(`./test-result/result-${case_name}.css`, (compiler(result)), function (err) {
-            if (err) {
-                console.log(`compile failed ${case_name}`)
-                return console.error(err);
-            }
-            console.log(`compile success ${case_name}`)
-        })
+
+        function write_compiled() {
+            fs.writeFile(path.join(cssDistPath, basename + '.css'), compiled, function (err) {
+                if (err) {
+                    return console.error(`compile failed ${basename}`);
+                }
+                console.log(`compile success ${basename}`)
+            })
+        }
+
+        if (!fs.existsSync(astDistPath)) {
+            console.log(astDistPath)
+            fs.mkdirSync(path.join(__dirname, astDistPath), { recursive: true })
+        }
+        if (!fs.existsSync(cssDistPath)) {
+            fs.mkdirSync(cssDistPath, { recursive: true })
+        }
+
+        write_ast()
+        write_compiled()
+    }
+    if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir, { recursive: true })
+    }
+
+    fs.readdirSync(inputDir).forEach((filename)=>{
+        if(filename === 'module') return;//Todos 先屏蔽模块化的compile
+        let filePath = path.join(inputDir, filename),
+            stat = fs.lstatSync(filePath)
+        if (stat.isFile()) {
+            return run_test(filePath)
+        }else if(stat.isDirectory()){
+            run(filePath)
+        }else{
+            console.error('Unknown file type')
+        }
     })
 }
+
+run('./test')
